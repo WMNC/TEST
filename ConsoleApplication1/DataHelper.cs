8a739483535933ffabbd5a2d2a1c5559c5fb33ac
WMNC <weim.nc@google.com>
2015-08-31 13:29:28 +0800

    TEST

---
+++ ConsoleApplication1/DataHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class DataHelper
+    {
+        private Object _lockObj = new object();
+        private DataHelper _instance = null;
+
+        public bool IsFriday()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static DataHelper GetImpl()
+        {
+            
+
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        #if CONDITION1
+        //[Conditional("CONDITION1")]
+        public static void Method1(int x)
+        {
+            Console.WriteLine("CONDITION1 is defined");
+        }
+        #endif
+
+        #if CONDITION1 && CONDITION2
+        [Conditional("CONDITION1"), Conditional("CONDITION2")]
+        public static void Method2()
+        {
+            Console.WriteLine("CONDITION1 or CONDITION2 is defined");
+        }
+        #endif
+
+    }
+}
